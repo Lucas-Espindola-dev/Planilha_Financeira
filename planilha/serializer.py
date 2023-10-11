e6c96ba0b39555cce1c35d0347c0317e1392011d
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 08:59:18 -0300

    Update serializer.py

--- planilha/serializer.py
+++ planilha/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from planilha.models import Receita, Despesa
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 
 class ReceitaSerializer(serializers.ModelSerializer):
@@ -12,3 +15,43 @@ class DespesaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Despesa
         fields = '__all__'
+
+
+class RegistraSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password])
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name',)
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': "Senhas não conferem!"}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
